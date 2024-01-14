ecba9f677a77cfeb0acb5e860be9056d1f2300a6
최지연 <T1100455@tmapmobility.com>
2024-01-15 00:25:22 +0900

    유아이 진행 중

--- lib/app/root/ui/main_view.dart
+++ lib/app/root/ui/main_view.dart
@@ -11,20 +11,33 @@ class MainView extends StatelessWidget {
     return Column(
       children: [
         Padding(
-          padding: edgeInsets(top: 40, bottom: 12, horizontal: 20),
-          child: Text('우  리  결  혼  합  니  다', style: textStyleBlack600(20)),
+          padding: edgeInsets(top: 80, bottom: 30, horizontal: 20),
+          child: Text('우 리 결 혼 합 니 다', style: textStyleBlack600(20)),
         ),
-        R.image.youngwoojiyeon1.image().paddingSymmetric(horizontal: 20),
+        Container(
+            constraints: const BoxConstraints(minWidth: 100, maxWidth: 700),
+            margin: edgeInsets(horizontal: 50),
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(16)),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: AspectRatio(
+                aspectRatio: 5/4,
+                child: R.image.youngwoojiyeon15.image(
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+        ),
         Padding(
           padding: edgeInsets(top: 30, bottom: 10, horizontal: 20),
-          child: Text('신영우 * 최지연', style: textStyleBlack600(30)),
+          child: Text('신영우 * 최지연', style: textStyleBlack600(20)),
         ),
         Padding(
-          padding: edgeInsets(bottom: 30, horizontal: 20),
+          padding: edgeInsets(bottom: 80, horizontal: 20),
           child: Text(
             '2024. 05. 04. 토요일 PM 3:00\n아펠가모 반포 LL층',
             textAlign: TextAlign.center,
-            style: textStyleBlack600(20),
+            style: textStyleBlack600(15),
           ),
         ),
       ],
