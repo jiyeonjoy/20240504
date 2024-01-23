90e8d3dae849eb0a564b3768e0b8d9ef5c210451
최지연 <choi02647@naver.com>
2024-01-24 00:16:59 +0900

    working

--- lib/app/root/controller/root_page_controller.dart
+++ lib/app/root/controller/root_page_controller.dart
@@ -1,10 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter_20240504/app/common/config/r.dart';
 import 'package:get/get.dart';
+import 'package:webview_flutter_platform_interface/webview_flutter_platform_interface.dart';
 
 class RootPageController extends GetxController {
   static RootPageController get to => Get.find();
 
-  var galleryList = [
+  final galleryList = [
     R.image.youngwoojiyeon1,
     R.image.youngwoojiyeon2,
     R.image.youngwoojiyeon3,
@@ -37,4 +40,96 @@ class RootPageController extends GetxController {
     R.image.youngwoojiyeon30,
     R.image.youngwoojiyeon31,
   ];
+
+  final PlatformWebViewController webViewController = PlatformWebViewController(
+    const PlatformWebViewControllerCreationParams(),
+  )..loadRequest(LoadRequestParams(
+      uri: Uri.parse(Uri.dataFromString(
+        '''
+      <!DOCTYPE html>
+<html>
+<head>
+  <script type="text/javascript" src='https://dapi.kakao.com/v2/maps/sdk.js?autoload=true&appkey=7047bd8eeaeb999d573881f23d55b160'></script>
+</head>
+<body style="padding:0; margin:0;">
+  <div id='map' style="width:300px;height:200px;margin: 0 auto;"/>
+	<script>
+	var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = { 
+        center: new kakao.maps.LatLng(37.50200137300353, 127.00096128071054), // 지도의 중심좌표
+        // center: new kakao.maps.LatLng(37.505088,127.034481),
+        level: 3,
+        draggable : false,
+        disableAutoPan: true
+    };
+
+var map = new kakao.maps.Map(mapContainer, mapOption);
+
+// 마커가 표시될 위치입니다 
+var markerPosition  = new kakao.maps.LatLng(37.50083002236247, 127.00319198989602);
+// var markerPosition  = new kakao.maps.LatLng(37.505088,127.034481);
+
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
+
+var iwContent = '<div style="padding:5px;">Hello World! <br><a href="https://map.kakao.com/link/map/Hello World!,33.450701,126.570667" style="color:blue" target="_blank">큰지도보기</a> <a href="https://map.kakao.com/link/to/Hello World!,33.450701,126.570667" style="color:blue" target="_blank">길찾기</a></div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+    iwPosition = new kakao.maps.LatLng(33.450701, 126.570667); //인포윈도우 표시 위치입니다
+
+// 인포윈도우를 생성합니다
+var infowindow = new kakao.maps.InfoWindow({
+    position : iwPosition, 
+    content : iwContent 
+});
+
+  
+// 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
+infowindow.open(map, marker);
+function relayout() {    
+    
+    // 지도를 표시하는 div 크기를 변경한 이후 지도가 정상적으로 표출되지 않을 수도 있습니다
+    // 크기를 변경한 이후에는 반드시  map.relayout 함수를 호출해야 합니다 
+    // window의 resize 이벤트에 의한 크기변경은 map.relayout 함수가 자동으로 호출됩니다
+    map.relayout();
+}
+		// var container = document.getElementById('map');
+	  // var latlon = new kakao.maps.LatLng(37.50083002236247, 127.00319198989602);
+	  // var options;
+    //
+    //   var imageSrc = 'https://jiyeonjoy.github.io/20240504/icons/Icon-192.png';
+    //   var imageSize = new kakao.maps.Size(44, 52);
+    //  
+    //   var imageOption = {offset: new kakao.maps.Point(27, 69)};
+    //         
+    //   // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+    //   var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+    //    
+    //   // 마커를 생성합니다
+    //   var marker = new kakao.maps.Marker({
+    //         position: latlon,
+    //         // image: markerImage, // 마커이미지 설정
+    //   });
+    //    
+    //   options = {
+    //     center: latlon,
+    //     level: 6,
+    //     disableDoubleClick: true,
+    //     draggable : false,
+    //   };
+    //  
+    //   var map = new kakao.maps.Map(container, options);
+    //   marker.setMap(map);		  
+	</script>
+</body>
+</html>
+    ''',
+        mimeType: 'text/html',
+        encoding: Encoding.getByName('utf-8'),
+      ).toString()),
+    )
+  );
 }
--- lib/app/root/ui/location_view.dart
+++ lib/app/root/ui/location_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_20240504/app/common/ui/edge_insets.dart';
 import 'package:flutter_20240504/app/common/ui/title_view.dart';
+import 'package:flutter_20240504/app/root/controller/root_page_controller.dart';
+import 'package:webview_flutter_platform_interface/webview_flutter_platform_interface.dart';
 
 class LocationView extends StatelessWidget {
   const LocationView({super.key});
@@ -9,12 +11,22 @@ class LocationView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         padding: edgeInsets(horizontal: 20, top: 50, bottom: 30),
-        child: const Column(
+        child: Column(
           children: [
-            TitleView('LOCATION'),
-            SizedBox(height: 30),
+            const TitleView('LOCATION'),
+            const SizedBox(height: 30),
+            Container(
+              constraints: const BoxConstraints(minWidth: 100, maxWidth: 500),
+              height: 400,
+              child: Center(
+                child: PlatformWebViewWidget(
+                  PlatformWebViewWidgetCreationParams(
+                      controller: RootPageController.to.webViewController,
+                  ),
+                ).build(context),
+              ),
+            )
           ],
-        )
-    );
+        ));
   }
 }
